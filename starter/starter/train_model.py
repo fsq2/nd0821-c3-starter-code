55bf65a0746b99c44d894fb23ef17c8940c1b177
fahad <43818190+fsq2@users.noreply.github.com>
2022-10-10 22:00:51 +0300

    remove prediction from training script

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pickle
 from sklearn.model_selection import train_test_split
 from ml.data import process_data
-from ml.model import train_model, inference, compute_model_metrics
+from ml.model import train_model, compute_model_metrics
 # Add the necessary imports for the starter code.
 file_dir = os.path.dirname(__file__)
 sys.path.insert(0, file_dir)
@@ -45,10 +45,3 @@ pickle.dump(encoder, open(encoder_path, 'wb'))
 
 lb_path = os.path.join(file_dir, '../model/lb.pkl')
 pickle.dump(lb, open(lb_path, 'wb'))
-
-# prediction
-preds = inference(rf_model, X_test)
-
-print('precision: {}, recall: {}, fbeta: {}'.format(
-    *compute_model_metrics(y_test, preds)
-))
